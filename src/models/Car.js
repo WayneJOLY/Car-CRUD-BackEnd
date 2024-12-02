64be12411fe644c1ea679e220acca25edb2c85a3
Gerald <geraldjoly191@gmail.com>
2024-12-02 12:36:22 -0300

    first commit and the model and his controllers are ready

---
+++ src/models/Car.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../utils/connection');
+
+const Car = sequelize.define('car', {
+    brand: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    model: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    color: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    year: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    price: {
+        type: DataTypes.FLOAT,
+        allowNull: false
+    },
+    image: {
+        type: DataTypes.TEXT,
+        allowNull: true
+    },
+});
+
+module.exports = Car;
